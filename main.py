434390b0a5b8a49abcd388049e503913de785b9f
PIERRE LOUIS Seth Luxama <pierrelouissethluxama@gmail.com>
2023-12-09 19:06:05 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+def kalkileditotal(longe, laje, kantite_moun):
+    distans_total = (longe + laje) * kantite_moun
+    return distans_total
+longe_kay = 10
+laje_kay = 20
+kantite_moun = 3
+
+distans_total = kalkileditotal(longe_kay, laje_kay, kantite_moun)
+print("Distans Total:", distans_total, "santimèt")
